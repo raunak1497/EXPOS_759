14a12cd605e747a7b8ae86da523bc093f5ac0285
Raunak Singh <raunak1497@gmail.com>
2019-10-24 17:35:09 +0530

    error in Stage 20

--- batch.bat
+++ batch.bat
@@ -1,19 +1,19 @@
-load --init $HOME/myexpos/expl/Samples/exec.xsm
+load --init $HOME/myexpos/expl/Samples/init.xsm
 load --idle $HOME/myexpos/expl/Samples/idle.xsm
 load --exec $HOME/myexpos/expl/Samples/llist.xsm
-load --exhandler ../spl/exhandler_20.xsm
-load --int=timer ../spl/timer_20.xsm
-load --int=6 ../spl/int6_20.xsm
-load --int=7 ../spl/int7_20.xsm
-load --int=8 ../spl/int8_20.xsm
-load --int=9 ../spl/int9_20.xsm
-load --int=10 ../spl/int10_20.xsm
-load --int=console ../spl/console_20.xsm
-load --int=disk ../spl/disk_20.xsm
-load --module 0 $HOME/myexpos/spl/mod0_20.xsm
-load --module 1 $HOME/myexpos/spl/mod1_20.xsm
-load --module 2 $HOME/myexpos/spl/mod2_20.xsm
-load --module 4 $HOME/myexpos/spl/mod4_20.xsm
-load --module 5 $HOME/myexpos/spl/mod5_20.xsm
-load --module 7 $HOME/myexpos/spl/boot_20.xsm
-load --os $HOME/myexpos/spl/os_19.xsm
+load --exhandler ../spl/exhandler.xsm
+load --int=timer ../spl/timer.xsm
+load --int=6 ../spl/int6.xsm
+load --int=7 ../spl/int7.xsm
+load --int=8 ../spl/int8.xsm
+load --int=9 ../spl/int9.xsm
+load --int=10 ../spl/int10.xsm
+load --int=console ../spl/console.xsm
+load --int=disk ../spl/disk.xsm
+load --module 0 $HOME/myexpos/spl/mod0.xsm
+load --module 1 $HOME/myexpos/spl/mod1.xsm
+load --module 2 $HOME/myexpos/spl/mod2.xsm
+load --module 4 $HOME/myexpos/spl/mod4.xsm
+load --module 5 $HOME/myexpos/spl/mod5.xsm
+load --module 7 $HOME/myexpos/spl/boot.xsm
+load --os $HOME/myexpos/spl/os.xsm
