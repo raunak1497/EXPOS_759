7a267197050b58ef37dc2eda5d7e7c75c25aa0a8
Raunak Singh <raunak1497@gmail.com>
2019-11-24 17:18:38 +0530

    Stage 24 completed

--- batch.bat
+++ batch.bat
@@ -5,9 +5,13 @@ load --exec $HOME/myexpos/expl/Samples/parent.xsm
 load --exec $HOME/myexpos/expl/Samples/child.xsm
 load --exec $HOME/myexpos/expl/Samples/assg23.xsm
 load --exec $HOME/myexpos/expl/Samples/assg23_2.xsm
+load --exec $HOME/myexpos/expl/Samples/assg24.xsm
+load --exec $HOME/myexpos/expl/Samples/assg24_2.xsm
+load --data $HOME/myexpos/expl/Samples/sample.dat
 load --exhandler ../spl/exhandler.xsm
 load --int=timer ../spl/timer.xsm
 load --int=4 ../spl/int4.xsm
+load --int=5 ../spl/int5.xsm
 load --int=6 ../spl/int6.xsm
 load --int=7 ../spl/int7.xsm
 load --int=8 ../spl/int8.xsm
@@ -22,6 +26,7 @@ load --int=disk ../spl/disk.xsm
 load --module 0 $HOME/myexpos/spl/mod0.xsm
 load --module 1 $HOME/myexpos/spl/mod1.xsm
 load --module 2 $HOME/myexpos/spl/mod2.xsm
+load --module 3 $HOME/myexpos/spl/mod3.xsm
 load --module 4 $HOME/myexpos/spl/mod4.xsm
 load --module 5 $HOME/myexpos/spl/mod5.xsm
 load --module 7 $HOME/myexpos/spl/boot.xsm
